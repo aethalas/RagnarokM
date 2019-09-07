c270c6e934fe08c4294de582d33b687c9caf3f47
Jonny Tyson <jonnytyson@live.com>
2019-09-06 20:10:48 -0700

    Initial

---
+++ src/components/RagnarokM.vue
@@ -0,0 +1,34 @@
+<template>
+ <div>
+   <div v-for="(entry, index) in monsters" :key="index">
+     {{entry.name}}
+   </div>
+ </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        monsters: [],
+        errors: []
+      }
+    },
+
+    created() {
+      axios.get('http://jonnyhtyson.com/ragnarokm/api/monster.php')
+      .then(response => {
+        this.monsters = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    }
+  }
+</script>
+
+<style>
+
+</style>
