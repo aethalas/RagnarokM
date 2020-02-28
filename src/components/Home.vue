9a8acf768d88b9c48a2511472aeba800dee1280f
Jonathon H. Tyson <jonnyhtyson@gmail.com>
2020-02-27 20:01:31 -0800

    small change

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
  <b-container fluid>
    <b-row>
      <b-col fluid="sm">Using Views! Much wow. So amaze.</b-col>
-     <b-col fluid="sm"><HomeRace/></b-col>
+     <b-col fluid="sm">Need to do some more wizardry later.</b-col>
    </b-row> 
 
    <b-row>
